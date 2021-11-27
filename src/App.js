34f68dd38fff606426f5d5b264af2dc63dac5c59
Blake Evans <32441662+BlakeEvans22@users.noreply.github.com>
2021-11-26 21:45:44 -0500

    Add Chakra-UI and start config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+    /* <div>
       <h1>Prepare for greatness</h1>
       <img
         style={{ width: "20vh", height: "auto" }}
         src="bagby.png"
         alt="Bag Bucks"
       />
-    </div>
+    </div> */
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "dark",
+  //useSystemColorMode: true,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
